829ad49a5fd31b3d3c60d5cda9bbc9b41af430c1
Sarthak Soumy <sarthaksoumy11@gmail.com>
2023-08-16 12:28:44 +0530

    Added a new test case for spacecraft to rotate right (failed).

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ class TestSpacecraftMethods(unittest.TestCase):
         spacecraft = Spacecraft(0, 0, 0, "N")
         spacecraft.move_forward()
         self.assertEqual(spacecraft.y, 1)
+    
+    def test_rotate_right(self):
+        spacecraft = Spacecraft(0, 0, 0, "N")
+        spacecraft.rotate_right()
+        self.assertEqual(spacecraft.direction, "E")
 
 if __name__ == '__main__':
     unittest.main()
