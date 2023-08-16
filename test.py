d912e46aca06324985a9f739a6084e861179879f
Sarthak Soumy <sarthaksoumy11@gmail.com>
2023-08-16 13:30:18 +0530

    Added test case to execute commands ( Initially Failed)

--- test.py
+++ test.py
@@ -52,6 +52,12 @@ class TestSpacecraftMethods(unittest.TestCase):
         spacecraft.rotate_left()
         self.assertEqual(spacecraft.direction, "S")
 
+    def test_execute_commands(self):
+        spacecraft = Spacecraft(0, 0, 0, "N")
+        commands = ["f", "r", "u", "b", "l"]
+        spacecraft.execute_commands(commands)
+        self.assertEqual(spacecraft.get_position(), (0, 1, -1))
+        self.assertEqual(spacecraft.get_direction(), "N")
 
     
 
